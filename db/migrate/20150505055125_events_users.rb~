0a6dc9b19e4a47298e0162e215d4eeb8a60ba404
thibau13 <thibau13@unlv.nevada.edu>
2015-05-08 21:13:30 -0700

    First Commit

---
+++ db/migrate/20150505055125_events_users.rb~
@@ -0,0 +1,4 @@
+class EventsUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create 3 Users
+User.create(name: "Brad")
+User.create(name: "Steven")
+User.create(name: "Stacey")
+
+# Create 3 Events
+Event.create(name: "Meeting")
+Event.create(name: "Presentation")
+Event.create(name: "Training")
+
+# Associate Events and Users as follows
+# event_id | user_id
+#-------------------
+#     1         1
+#     1         2
+
+Event.find(1).users << User.find(1)
+Event.find(2).users << User.find(1)
+Event.find(3).users << User.find(1)
+
+Event.find(1).users << User.find(2)
+Event.find(2).users << User.find(2)
+
+Event.find(1).users << User.find(3)
